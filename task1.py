14ef7ad4263f7329a2477e8564c905774af43c13
Aashik <138803558+aashikv@users.noreply.github.com>
2023-07-18 11:20:12 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,7 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> radius = float(input("Enter the radius of the circle: "))
+Enter the radius of the circle: 3.5
+>>> area = 3.14159 * (radius ** 2)
+>>> print("The area of the circle is:", area)
+The area of the circle is: 38.4844775
